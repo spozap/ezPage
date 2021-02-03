bb8ac9aaba569a74e86b567db2d77e9f553dd82f
spozap <sergi.poza@iesjoandaustria.org>
2021-02-03 17:25:16 +0100

    swagger setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
 const app = express()
 const dbConnection = require('./config/dbConnection')
 
@@ -8,6 +10,23 @@ const port = 3000
 dbConnection()
 
 app.use(bodyParser.json({type: 'application/json' })) // Parse body os requests to JSON
+
+const swaggerDefinition = {
+    openapi: '3.0.2',
+    info: {
+        title: "ezPage API",
+        version: "1.0.0"
+    },
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['./server/src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsDoc(options)
+
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
 
 app.get('/',(req,res) => {
     res.send(`Connection was successful`)
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -4,6 +4,27 @@ module.exports = app => {
 
     const router = require('express').Router()
 
+    /**
+     * @swagger
+     * paths:
+     *  /users:
+     *   post:
+     *    summary: Creates a user
+     *    requestBody:
+     *     required: true
+     *     content:
+     *      application/json:
+     *       schema:
+     *        type: object
+     *        properties:
+     *         username:
+     *          type: string
+     *         password:
+     *          type: string
+     *    responses:
+     *     201:
+     *      description: created
+     */
     router.post('/',controller.create)
 
     app.use('/users',router);
