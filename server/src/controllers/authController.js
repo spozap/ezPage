dc3d0b7b47e84f9ef7f747ecfefab14dc5493b73
spozap <sergi.poza@iesjoandaustria.org>
2021-03-11 16:55:09 +0100

    register endpoint

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 const model = require('../models/user')
 const bcrypt = require('bcrypt')
 const { createToken } = require('../services/token')
+const userModel = require('../models/user')
+
+const saltRounds = 10
 
 let controller = {}
 
@@ -40,8 +43,37 @@ controller.login = async (req,res) => {
         
 
     } catch(err) {
-        console.log(err)
+        res.status(500).send(err)
     }
+
+}
+
+controller.register = async (req,res) => {
+
+    if (!req.body.username || !req.body.password) {
+        res.status(400).send("Username or password empty")
+        return
+    }
+
+    try {
+
+        const cmp = await model.findOne({ username: req.body.username })
+
+        if (cmp) {
+            res.status(409).send("User already exists")
+            return
+        }
+
+        const hashedPwd = await bcrypt.hash(req.body.password, saltRounds)
+        let user = new userModel({username: req.body.username , password: hashedPwd})
+        
+        user.save()
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+
+    res.status(201).send("User registered successfully")
 
 }
 
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -32,5 +32,33 @@ module.exports = app => {
      */
     router.post('/login',controller.login)
 
+    /**
+     * @swagger
+     *  /auth/register:
+     *    post:
+     *     summary: Validates a user and returns JWT token if user is valid
+     *     requestBody:
+     *      required: true
+     *      content:
+     *       application/json:
+     *        schema:
+     *         type: object
+     *         properties:
+     *          username:
+     *           type: string
+     *          password:
+     *           type: string
+     *     responses:
+     *      '200':
+     *        description: User registered
+     *      '409':
+     *        description: User already exists
+     *      '400':
+     *        description: Malformed request
+     *      '500':
+     *        description: Server problem
+     */    
+    router.post('/register',controller.register)
+
     app.use('/auth',router)
 }
